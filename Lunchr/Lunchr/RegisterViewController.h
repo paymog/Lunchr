4e8310c02c46ced6c599d19e97348d0cd6e447d9
Sean Midford <seanmidford@gmail.com>
2015-02-24 14:43:01 -0600

    Initial storyboard and basic view controller setup.

---
+++ Lunchr/Lunchr/RegisterViewController.h
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.h
+//  Lunchr
+//
+//  Created by Sean-MacbookPro on 2/24/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITextField *firstField;
+@property (strong, nonatomic) IBOutlet UITextField *lastField;
+@property (strong, nonatomic) IBOutlet UITextField *emailField;
+@property (strong, nonatomic) IBOutlet UITextField *passField;
+@property (strong, nonatomic) IBOutlet UITextField *verifyField;
+
+@end
---
+++ Lunchr/Lunchr/RegisterViewController.m
@@ -0,0 +1,38 @@
+//
+//  RegisterViewController.m
+//  Lunchr
+//
+//  Created by Sean-MacbookPro on 2/24/15.
+//
+//
+
+#import "RegisterViewController.h"
+
+@interface RegisterViewController ()
+
+@end
+
+@implementation RegisterViewController
+
+@synthesize firstField;
+@synthesize lastField;
+@synthesize emailField;
+@synthesize passField;
+@synthesize verifyField;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)registerButtonTapped:(id)sender {
+    
+}
+
+
+@end
