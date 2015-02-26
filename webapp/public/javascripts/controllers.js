274c4309eb09184dade665c972eaab83acf249b9
Brandon Wade <brandon_wade2005@hotmail.com>
2015-02-26 15:25:43 -0600

    Added support for geolocation from browser.
    Loading map page now automatically prompts user for their location.
    If the user accepts, the embedded map will scroll to their current
    location.`

--- webapp/public/javascripts/controllers.js
+++ webapp/public/javascripts/controllers.js
@@ -67,4 +67,17 @@ lunchrControllers.controller('MapController', ['$scope', '$http', '$state',
 
         $scope.map = { center: { latitude: 49.8651559, longitude: -97.11077669999997 }, zoom: 14 };
 
+        if ( navigator.geolocation )
+        {
+            navigator.geolocation.getCurrentPosition( function ( position )
+            {
+                $scope.$apply( function( )
+                {
+                    $scope.position = position;
+
+                    $scope.map = { center: { latitude: position.coords.latitude, longitude: position.coords.longitude }, zoom: 14 };
+                    $scope.map.flush( );
+                } );
+            } );
+        }
     }]);
