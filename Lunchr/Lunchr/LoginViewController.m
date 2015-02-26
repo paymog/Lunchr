46033bb9a0789e99eda71697c7a5da933e286be4
Sean Midford <seanmidford@gmail.com>
2015-02-26 14:58:17 -0600

    User authentication with http POST.

--- Lunchr/Lunchr/LoginViewController.m
+++ Lunchr/Lunchr/LoginViewController.m
@@ -27,17 +27,18 @@
 - (IBAction)loginButtonTapped:(UIButton *)sender {
     
     // Show error label when input is incorrect
-    if (![self validateLogin]) {
+    // Validate login info and attempt to login user
+    if (![self validateLogin] || ![self loginUser]) {
         
         [self.errorLabel setHidden:FALSE];
+        [self.passField setText:@""];
+    
     } else {
         
         [self.errorLabel setHidden:TRUE];
         
-        // Login user
-        
         // Move to next view
-        [self performSegueWithIdentifier:@"mySegue" sender:sender];
+        [self performSegueWithIdentifier:@"loginSegue" sender:sender];
     }
 }
 
@@ -51,6 +52,45 @@
     } else {
         
         return true;
+    }
+}
+
+- (bool)loginUser {
+    
+    @try {
+        
+//        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://54.200.225.219/api/users/authenticate"]];
+        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://localhost:3000/api/users/authenticate"]];
+        NSDictionary *jsonDictionary = [NSDictionary dictionaryWithObjectsAndKeys:
+                                        emailField.text, @"email",
+                                        passField.text, @"password",
+                                        nil];
+        NSError *error = nil;
+
+        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:jsonDictionary options:NSJSONWritingPrettyPrinted error:&error];
+        NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+        NSUInteger postLength = [jsonString length];
+        
+        [request setHTTPMethod:@"POST"];
+        [request setValue:@"application/json" forHTTPHeaderField:@"Content-type"];
+        [request setValue:[NSString stringWithFormat:@"%d", (int)postLength] forHTTPHeaderField:@"Content-length"];
+        [request setHTTPBody:[jsonString dataUsingEncoding:NSUTF8StringEncoding]];
+        
+        NSHTTPURLResponse *resp;
+        [NSURLConnection sendSynchronousRequest:request returningResponse:&resp error:&error];
+        
+        if (resp != nil && [resp statusCode] == 200) {
+            
+            return true;
+            
+        } else {
+        
+            return false;
+        }
+    
+    } @catch (NSException *e) {
+        
+        return false;
     }
 }
 
