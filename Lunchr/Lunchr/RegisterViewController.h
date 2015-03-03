c176af61920c2e75678177a91fa4364a33c049ce
Sean Midford <seanmidford@gmail.com>
2015-03-03 16:12:54 -0600

    Added server requests. Moved http request code to new file. Added files for tests.

--- Lunchr/Lunchr/RegisterViewController.h
+++ Lunchr/Lunchr/RegisterViewController.h
@@ -15,5 +15,6 @@
 @property (strong, nonatomic) IBOutlet UITextField *emailField;
 @property (strong, nonatomic) IBOutlet UITextField *passField;
 @property (strong, nonatomic) IBOutlet UITextField *verifyField;
+@property (strong, nonatomic) IBOutlet UILabel *errorLabel;
 
 @end
