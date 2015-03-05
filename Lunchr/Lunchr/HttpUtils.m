27b5b777549dc26075c1c4b4fca553352213249c
Sean Midford <seanmidford@gmail.com>
2015-03-05 14:53:42 -0600

    Changed field names. Removed duplicate code.

--- Lunchr/Lunchr/HttpUtils.m
+++ Lunchr/Lunchr/HttpUtils.m
@@ -10,92 +10,71 @@
 
 @implementation HttpUtils
 
++ (bool)postRequest:(NSDictionary *)jsonDictionary
+              toURL:(NSString *)url{
+    
+    
+    @try {
+
+        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url]];
+        NSError *error = nil;
+        
+        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:jsonDictionary options:NSJSONWritingPrettyPrinted error:&error];
+        NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+        NSUInteger postLength = [jsonString length];
+        
+        [request setHTTPMethod:@"POST"];
+        [request setValue:@"application/json" forHTTPHeaderField:@"Content-type"];
+        [request setValue:[NSString stringWithFormat:@"%d", (int)postLength] forHTTPHeaderField:@"Content-length"];
+        [request setHTTPBody:[jsonString dataUsingEncoding:NSUTF8StringEncoding]];
+        
+        NSHTTPURLResponse *resp;
+        [NSURLConnection sendSynchronousRequest:request returningResponse:&resp error:&error];
+        
+        if (resp != nil && [resp statusCode] == 200) {
+            
+            return true;
+            
+        } else {
+            
+            return false;
+        }
+    
+    } @catch (NSException *e) {
+        
+        return false;
+    }
+    
+}
+
 + (bool)registerUser:(NSString *)email
           toPassword:(NSString *)password
          toFirstName:(NSString *)firstName
           toLastName:(NSString *)lastName {
     
-    @try {
-        
-        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://54.200.225.219/api/users/register"]];
-        //NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://localhost:3000/api/users/register"]];
-        
-        NSDictionary *jsonDictionary = [NSDictionary dictionaryWithObjectsAndKeys:
-                                        email, @"email",
-                                        password, @"password",
-                                        firstName, @"firstname",
-                                        lastName, @"lastname",
-                                        nil];
-        NSError *error = nil;
-        
-        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:jsonDictionary options:NSJSONWritingPrettyPrinted error:&error];
-        NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-        NSUInteger postLength = [jsonString length];
-        
-        [request setHTTPMethod:@"POST"];
-        [request setValue:@"application/json" forHTTPHeaderField:@"Content-type"];
-        [request setValue:[NSString stringWithFormat:@"%d", (int)postLength] forHTTPHeaderField:@"Content-length"];
-        [request setHTTPBody:[jsonString dataUsingEncoding:NSUTF8StringEncoding]];
-        
-        NSHTTPURLResponse *resp;
-        [NSURLConnection sendSynchronousRequest:request returningResponse:&resp error:&error];
-        
-        if (resp != nil && [resp statusCode] == 200) {
-            
-            return true;
-            
-        } else {
-            
-            return false;
-        }
-        
-    } @catch (NSException *e) {
-        
-        return false;
-    }
+    NSDictionary *jsonDictionary = [NSDictionary dictionaryWithObjectsAndKeys:
+                                    email, @"email",
+                                    password, @"password",
+                                    firstName, @"firstname",
+                                    lastName, @"lastname",
+                                    nil];
+    NSString *url = @"http://54.200.225.219/api/users/register";
     
-    return true;
+    return [HttpUtils postRequest:jsonDictionary toURL:url];
+    
 }
 
 + (bool)loginUser:(NSString *)email
        toPassword:(NSString *)password {
     
-    @try {
-        
-        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://54.200.225.219/api/users/authenticate"]];
-        //NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://localhost:3000/api/users/authenticate"]];
-        
-        NSDictionary *jsonDictionary = [NSDictionary dictionaryWithObjectsAndKeys:
-                                        email, @"email",
-                                        password, @"password",
-                                        nil];
-        NSError *error = nil;
-        
-        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:jsonDictionary options:NSJSONWritingPrettyPrinted error:&error];
-        NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-        NSUInteger postLength = [jsonString length];
-        
-        [request setHTTPMethod:@"POST"];
-        [request setValue:@"application/json" forHTTPHeaderField:@"Content-type"];
-        [request setValue:[NSString stringWithFormat:@"%d", (int)postLength] forHTTPHeaderField:@"Content-length"];
-        [request setHTTPBody:[jsonString dataUsingEncoding:NSUTF8StringEncoding]];
-        
-        NSHTTPURLResponse *resp;
-        [NSURLConnection sendSynchronousRequest:request returningResponse:&resp error:&error];
-        
-        if (resp != nil && [resp statusCode] == 200) {
-            
-            return true;
-            
-        } else {
-            
-            return false;
-        }
-        
-    } @catch (NSException *e) {
-        
-        return false;
-    }
+    NSDictionary *jsonDictionary = [NSDictionary dictionaryWithObjectsAndKeys:
+                                    email, @"email",
+                                    password, @"password",
+                                    nil];
+    NSString *url = @"http://54.200.225.219/api/users/authenticate";
+    
+    
+    return [HttpUtils postRequest:jsonDictionary toURL:url];
 }
 
 @end
