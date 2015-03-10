f9c33437927ec9c17b19a956e8ddca4123131667
Paymahn Moghadasian <paymahn1@gmail.com>
2015-03-09 23:10:53 -0500

    Current user didn't persist through reloads. Use cookies to fix.
    Use ngCookies to persist the current user through reloads and navigating
    away from the app.

--- webapp/public/javascripts/services.js
+++ webapp/public/javascripts/services.js
@@ -1,19 +1,22 @@
 'use strict';
 
-var lunchrServices = angular.module('lunchrServices', []);
+var lunchrServices = angular.module('lunchrServices', ['ngCookies']);
 
-lunchrServices.service('authService', function () {
-    var user = null;
+lunchrServices.service('authService', ['$cookies', function ($cookies) {
+    var user = $cookies.lunchr_currentUser || null;
 
     this.login = function (userEmail) {
         user = userEmail;
+        $cookies.lunchr_currentUser = userEmail;
     };
+    
     this.logout = function () {
         user = null;
+        $cookies.lunchr_currentUser = null;
     };
-    this.currentUser = function(){
+
+    this.currentUser = function () {
         return user;
     }
-
-});
+}]);
 
