fe592aecf770c665520f1430fe4c33703a4efd33
kjrcda <kjrcda@hotmail.com>
2015-03-10 13:46:09 -0500

    Quick test cleanup
    Got rid of extra parameters in main controller test. Made a test file
    for Home Page Controller

---
+++ webapp/ng_tests/controllers/home_page_controller_test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+describe('HomePageController', function () {
+    beforeEach(module('lunchr'));
+
+    var DEFAULT_NAME = 'Amy';
+
+    var $controller, $rootScope;
+
+    function createController(stateParams) {
+        return $controller('UserMatchedController', {$scope: $rootScope, $stateParams: stateParams});
+    }
+
+    beforeEach(inject(function ($injector) {
+
+        // Get hold of a scope (i.e. the root scope)
+        $rootScope = $injector.get('$rootScope');
+
+        // The $controller service is used to create instances of controllers
+        $controller = $injector.get('$controller');
+    }));
+
+    describe('on initialization', function() {
+        it('sets $scope.name based on $stateParams', function() {
+            createController({name: DEFAULT_NAME});
+
+            expect($rootScope.name).toBe(DEFAULT_NAME);
+        })
+    })
+});
